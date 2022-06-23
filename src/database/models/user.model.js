45823f0a77774154c793ffc6ef7e6f302ab8b6c6
get0r <jahsirtg@protonmail.com>
2022-06-23 14:41:17 +0300

    user.model-- add user profile schema/model

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -1,28 +1,57 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 3,
-    maxlength: 255,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 3,
-    maxlength: 100,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
+    },
 
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 255,
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
+    },
+
+    ppLink: String,
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 255,
+    },
+
+    bio: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 const UserModel = mongoose.model('User', userSchema);
 
