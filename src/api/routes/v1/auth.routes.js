b4deb6a142056944b107aeb68062b177bcef0102
get0r <jahsirtg@protonmail.com>
2022-06-24 11:37:37 +0300

    auth.routes-- refactor with joi validator

--- src/api/routes/v1/auth.routes.js
+++ src/api/routes/v1/auth.routes.js
@@ -1,26 +1,27 @@
 const express = require('express');
 
-const AuthController = require('../../controller/auth.controller');
-const AuthValidator = require('../../middlewares/validation/auth.validator');
-
+const { signUpSchema, signInSchema } = require('../../../database/validationSchemas/auth.joi.schema');
+const { validateAsync } = require('../../middlewares/validation/joi.validator');
 const { AUTH_ROUTES } = require('./constants/route.constants');
+
+const AuthController = require('../../controller/auth.controller');
 
 const authRouter = express.Router();
 
 /* A post request to the route `/api/v1/auth/signup` and it is using the
 `AuthValidator.validateUserSignUp` middleware and the `AuthController.userSignUp` controller. */
 authRouter
-  .post(AUTH_ROUTES.SIGNUP, AuthValidator.validateUserSignUp, AuthController.userSignUp);
+  .post(AUTH_ROUTES.SIGNUP, validateAsync(signUpSchema), AuthController.userSignUp);
 
 /* A post request to the route `/api/v1/auth/signin` and it is using the
 `AuthValidator.validateUserSignIn` middleware and the `AuthController.userSignIn` controller. */
 authRouter
-  .post(AUTH_ROUTES.SIGNIN, AuthValidator.validateUserSignIn, AuthController.userSignIn);
+  .post(AUTH_ROUTES.SIGNIN, validateAsync(signInSchema), AuthController.userSignIn);
 
 authRouter
-  .post(AUTH_ROUTES.FORGOT_PASSWORD, AuthValidator.validateUserSignIn, AuthController.userSignIn);
+  .post(AUTH_ROUTES.FORGOT_PASSWORD, AuthController.userSignIn);
 
 authRouter
-  .post(AUTH_ROUTES.VERIFY, AuthValidator.validateUserSignIn, AuthController.userSignIn);
+  .post(AUTH_ROUTES.VERIFY, AuthController.userSignIn);
 
 module.exports = authRouter;
