75e5358618d24a571d0374022199acd3c691475b
get0r <jahsirtg@protonmail.com>
2022-06-24 14:59:28 +0300

    third-party-- integrate mailgun verification email sender

--- src/config/index.js
+++ src/config/index.js
@@ -5,4 +5,6 @@ module.exports = {
   dbURI: process.env.DB_URI,
   tokenSecret: process.env.TOKEN_SECRET,
   port: process.env.PORT,
+  mailgunAPIKEY: process.env.MAILGUN_API_KEY,
+  mailgunDomain: process.env.MAILGUN_DOMAIN,
 };
---
+++ src/services/third-party/email.service.js
@@ -0,0 +1,41 @@
+const Mailgun = require('mailgun.js');
+const formData = require('form-data');
+const { appLogger } = require('../../helpers/logger/appLogger');
+const { mailgunAPIKEY, mailgunDomain } = require('../../config');
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({ username: 'api', key: mailgunAPIKEY });
+
+const sendVerificationEmail = async (emailAddress, token) => {
+  try {
+    // const compiledFunction = pug
+    // .compileFile(path.join(__dirname, '/../../assets/templates/jobsAlertTemplate.pug'));
+    // const jobsListHTML = compiledFunction({
+    //   jobs,
+    // });
+    const verificationLink = `http://localhost:4000/api/v1/auth/verify/${token}`;
+
+    const messageData = {
+      from: 'Glumos <noreply@glumos.com>',
+      to: emailAddress,
+      subject: 'Verify Your Account',
+      text: verificationLink,
+    };
+
+    await client.messages.create(mailgunDomain, messageData);
+
+    appLogger.info(`Sent verification email to ${emailAddress}`);
+    return true;
+  } catch (error) {
+    if (error.name === 'TimeoutError') {
+      appLogger.error(`retrying sending email to ${emailAddress} list due to Timeout error`);
+      return sendVerificationEmail(emailAddress, token);
+    }
+    appLogger.error(`error while sending email to ${emailAddress} ${JSON.stringify(error)}`);
+  }
+  return false;
+};
+
+module.exports = {
+  sendVerificationEmail,
+};
