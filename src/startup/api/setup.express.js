0300fb60eebc17dbc1ded2001d8f7bdc4e0f3bea
get0r <jahsirtg@protonmail.com>
2022-06-26 17:48:49 +0300

    setup.express-- cors disabled version

--- src/startup/api/setup.express.js
+++ src/startup/api/setup.express.js
@@ -30,10 +30,10 @@ module.exports = (app) => {
   //  strip any database related chars from requests for security.
   app.use(ExpressMongoSanitize());
 
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
+  // app.use(cors({
+  //   origin: 'http://localhost:3000',
+  //   credentials: true,
+  // }));
 
   //  network traffic logger.
   app.use(httpLogger);
